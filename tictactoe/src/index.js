86738f41928c029fafb1956ca9ba03a92e80019b
Tiffany B <tjbrincefield@gmail.com>
2019-10-07 12:54:44 -0500

    added state prop into board component

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -12,8 +12,9 @@ class Square extends React.Component {
     }
     render() {
       return (
-        <button className="square" onClick={() =>
-            this.setState({value: 'X'}) }>
+        <button className="square" 
+        onClick={() => this.setState({value: 'X'}) }
+        >
           {this.state.value}
         </button>
       );
@@ -21,6 +22,12 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+      constructor(props) {
+          super(props);
+          this.state = {
+              squares: Array(9).fill(null),
+          };
+      }
     renderSquare(i) {
       return <Square value={i} />;
     }
